6056cb227c9f9c645614731ce0a5693aedb34fb4
sagorwebdeveloper <67562467+sagorwebdeveloper@users.noreply.github.com>
2023-04-02 06:51:38 +0600

    Create getData.java

---
+++ src/CineMagic/getData.java
@@ -0,0 +1,13 @@
+package CineMagic;
+
+public class getData {
+
+    public static String username;
+
+    public static String path;
+
+    public static Integer movieId;
+
+    public static String title;
+
+}
