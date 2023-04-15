b4258e4e47747b8b3b9ada3c7ff97deb26d6c44f
Atikur7874 <53107817+Atikur7874@users.noreply.github.com>
2023-04-16 04:55:28 +0600

    Create customerData.java

---
+++ src/CineMagic/customerData.java
@@ -0,0 +1,53 @@
+package CineMagic;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class customerData {
+
+    private Integer id;
+    private String type;
+    private String title;
+    private Integer quantity;
+    private double total;
+    private Date date;
+    private Time time;
+
+    public customerData(Integer id, String type, String title, Integer quantity, double total, Date date, Time time) {
+        this.id = id;
+        this.type = type;
+        this.title = title;
+        this.quantity = quantity;
+        this.total = total;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+}
